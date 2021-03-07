f923a7f47f89b8fb586128d72372a1265808fdf5
9yelin9 <you@example.com>
2021-03-08 01:33:23 +0900

    Update

---
+++ sh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -q single.q
+#$ -v OMP_NUM_THREADS=1
+#$ -cwd
+#$ -j y
+
+echo "nodes list"
+cat ${PE_HOSTFILE}
+pwd
+echo "--------------------------------"
+echo "jobid:       ${PE_JOBID}"
+echo "nodes:       ${NNODES}"
+echo "cores:       ${NCORES}"
+echo "Nodefile:    ${PE_HOSTFILE}"
+echo "# time-start "`date`
+echo "--------------------------------"
+
+##source ~/.bashrc
+
+./
